69ffd1d5be7dcc22b58a1188565de987404a8fa0
Slowhand0309 <slowhand0309@gmail.com>
2016-11-17 23:15:15 +0900

    Add macro for color

--- src/include/ATgae/engine/com/ATgaeCommon.h
+++ src/include/ATgae/engine/com/ATgaeCommon.h
@@ -4,6 +4,29 @@
 #include "ATgae/debugger/ATgaeLog.h"
 
 /**
+ * Declare.
+ */
+#define ATCOLOR_ARGB(a, r, g, b)  ((ATColor)((((a)&0x000000FF)<<24)|(((r)&0x000000FF)<<16)|(((g)&0x000000FF)<<8)|((b)&0x000000FF)))
+#define ATCOLOR_RGBA(r, g, b, a)  ATCOLOR_ARGB(a, r, g, b)
+
+#define ATCOLOR_A(color)          (int)(color >> 24 & 0x000000FF)
+#define ATCOLOR_R(color)          (int)(color >> 16 & 0x000000FF)
+#define ATCOLOR_G(color)          (int)(color >> 8 & 0x000000FF)
+#define ATCOLOR_B(color)          (int)(color & 0x000000FF)
+
+#define ATCOLOR_RED               ATCOLOR_ARGB(255, 255, 0, 0)
+#define ATCOLOR_GREEN             ATCOLOR_ARGB(255, 0, 255, 0)
+#define ATCOLOR_BLUE              ATCOLOR_ARGB(255, 0, 0, 255)
+#define ATCOLOR_YELLOW            ATCOLOR_ARGB(255, 255, 255, 0)
+#define ATCOLOR_WHITE             ATCOLOR_ARGB(255, 255, 255, 255)
+#define ATCOLOR_BLACK             ATCOLOR_ARGB(255, 0, 0, 0)
+#define ATCOLOR_PINK              ATCOLOR_ARGB(255, 255, 192, 203)
+#define ATCOLOR_ORANGE            ATCOLOR_ARGB(255, 255, 165, 0)
+#define ATCOLOR_GRAY              ATCOLOR_ARGB(255, 128, 128, 128)
+#define ATCOLOR_BROWN             ATCOLOR_ARGB(255, 165, 42, 42)
+#define ATCOLOR_VIOLET            ATCOLOR_ARGB(255, 238, 130, 238)
+
+/**
  * Macro.
  */
 
